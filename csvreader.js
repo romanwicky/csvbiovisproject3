dd7c0ebfb43336aa24d841c5ac79fef1f5e18da7
Roman Wicky <rwickyvd@gmail.com>
2021-02-25 20:29:06 -0500

    overhaul

--- csvreader.js
+++ csvreader.js
@@ -1,31 +1,69 @@
+const fs = require('fs');
+const Papa = require('papaparse');
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+let sdf;
+let errorPec = [];
 
-var today = new Date();
-var dd = String(today.getDate()).padStart(2, '0');
-var mm = String(today.getMonth() + 1).padStart(2, '0'); // January is 0!
-var yyyy = today.getFullYear();
-var minutes = today.getMinutes();
+function createCSV(){
+    let today = new Date();
+    let dd = String(today.getDate()).padStart(2, '0');
+    let mm = String(today.getMonth() + 1).padStart(2, '0'); // January is 0!
+    let yyyy = today.getFullYear();
+    let minutes = today.getMinutes();
 
-today = mm + dd  + yyyy + minutes;
+    today = mm + dd  + yyyy + minutes;
 
-let todayString = today.toString();
+    let todayString = today.toString();
+    const csvWriter = createCsvWriter({
+        path: 'csvdata/' + todayString + '.csv',
+        header: [
+            {id: 'ParticipantID', title: 'ParticipantID'},
+            {id: 'TrialNum', title: 'TrialNum'},
+            {id: 'Vis', title: 'Vis'},
+            {id: 'TruePercent', title: 'TruePercent'},
+            {id: 'ReportedPercent', title: 'ReportedPercent'},
+        ]
+    });
+
+    data = [ { "ParticipantID": "Test21", "TrialNum": 0, "Vis": "barchart_vert", "TruePercent": "5", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 1, "Vis": "barchart_vert", "TruePercent": "1.3157894736842106", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 2, "Vis": "barchart_vert", "TruePercent": "1.8333333333333333", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 3, "Vis": "barchart_vert", "TruePercent": "1.1136363636363635", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 4, "Vis": "barchart_vert", "TruePercent": "1.0344827586206897", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 5, "Vis": "barchart_vert", "TruePercent": "4", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 6, "Vis": "barchart_vert", "TruePercent": "1.3", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 7, "Vis": "barchart_vert", "TruePercent": "1.3823529411764706", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 8, "Vis": "barchart_vert", "TruePercent": "3.6923076923076925", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 9, "Vis": "barchart_vert", "TruePercent": "1.2857142857142858", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 10, "Vis": "barchart_hor", "TruePercent": "1.0555555555555556", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 11, "Vis": "barchart_hor", "TruePercent": "1.1627906976744187", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 12, "Vis": "barchart_hor", "TruePercent": "1.9523809523809523", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 13, "Vis": "barchart_hor", "TruePercent": "4.666666666666667", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 14, "Vis": "barchart_hor", "TruePercent": "1.4", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 15, "Vis": "barchart_hor", "TruePercent": "2.823529411764706", "ReportedPercent": "123123" }, { "ParticipantID": "Test21", "TrialNum": 16, "Vis": "barchart_hor", "TruePercent": "1.3333333333333333", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 17, "Vis": "barchart_hor", "TruePercent": "7.666666666666667", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 18, "Vis": "barchart_hor", "TruePercent": "1.8666666666666667", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 19, "Vis": "barchart_hor", "TruePercent": "1.2352941176470589", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 0, "Vis": "angles", "TruePercent": "341", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 1, "Vis": "angles", "TruePercent": "15", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 2, "Vis": "angles", "TruePercent": "16", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 3, "Vis": "angles", "TruePercent": "160", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 4, "Vis": "angles", "TruePercent": "32", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 5, "Vis": "angles", "TruePercent": "189", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 6, "Vis": "angles", "TruePercent": "140", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 7, "Vis": "angles", "TruePercent": "48", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 8, "Vis": "angles", "TruePercent": "347", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 9, "Vis": "angles", "TruePercent": "239", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 10, "Vis": "angles", "TruePercent": "100", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 11, "Vis": "angles", "TruePercent": "17", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 12, "Vis": "angles", "TruePercent": "44", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 13, "Vis": "angles", "TruePercent": "17", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 14, "Vis": "angles", "TruePercent": "347", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 15, "Vis": "angles", "TruePercent": "269", "ReportedPercent": "123123" }, { "ParticipantID": "Test21", "TrialNum": 16, "Vis": "angles", "TruePercent": "319", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 17, "Vis": "angles", "TruePercent": "187", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 18, "Vis": "angles", "TruePercent": "318", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 19, "Vis": "angles", "TruePercent": "124", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 0, "Vis": "volume", "TruePercent": "144", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 1, "Vis": "volume", "TruePercent": "64", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 2, "Vis": "volume", "TruePercent": "16", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 3, "Vis": "volume", "TruePercent": "25", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 4, "Vis": "volume", "TruePercent": "81", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 5, "Vis": "volume", "TruePercent": "100", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 6, "Vis": "volume", "TruePercent": "36", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 7, "Vis": "volume", "TruePercent": "49", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 8, "Vis": "volume", "TruePercent": "81", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 9, "Vis": "volume", "TruePercent": "100", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 10, "Vis": "volume", "TruePercent": "169", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 11, "Vis": "volume", "TruePercent": "121", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 12, "Vis": "volume", "TruePercent": "100", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 13, "Vis": "volume", "TruePercent": "81", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 14, "Vis": "volume", "TruePercent": "100", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 15, "Vis": "volume", "TruePercent": "16", "ReportedPercent": "123123" }, { "ParticipantID": "Test21", "TrialNum": 16, "Vis": "volume", "TruePercent": "64", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 17, "Vis": "volume", "TruePercent": "16", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 18, "Vis": "volume", "TruePercent": "25", "ReportedPercent": "123" }, { "ParticipantID": "Test21", "TrialNum": 19, "Vis": "volume", "TruePercent": "25", "ReportedPercent": "123" } ]
+    csvWriter
+        .writeRecords(data)
+        .then(()=> console.log('The CSV file was written successfully'));
+}
 
 
-const csvWriter = createCsvWriter({
-    path: 'csvdata/' + todayString + '.csv',
-    header: [
-        {id: 'ParticipantID', title: 'ParticipantID'},
-        {id: 'TrialNum', title: 'TrialNum'},
-        {id: 'Vis', title: 'Vis'},
-        {id: 'TruePercent', title: 'TruePercent'},
-        {id: 'ReportedPercent', title: 'ReportedPercent'},
-    ]
-});
+function getCSVDataCalcError(){
+    const file = fs.createReadStream('csvdata/022520211.csv');
+
+    function test(data){
+        sdf = data;
+        for(let i = 0; i < sdf.length; i++){
+            let truePercent = sdf[i].TruePercent;
+            let reportedPercent = sdf[i].ReportedPercent;
+            console.log(truePercent, reportedPercent);
+            let visType = sdf[i].Vis;
+            let ratio = Math.log2(Math.abs(reportedPercent - truePercent) + (1/8));
+
+            errorPec.push([visType, ratio]);
+        }
+        console.log(errorPec)
+    }
+
+    Papa.parse(file, {
+        header: true,
+        dynamicTyping: true,
+        complete: function(results) {
+            //console.log("Finished:", results.data);
+            test(results.data);
+            sdf = results.data;
+        }
+    });
+
+}
+
+getCSVDataCalcError()
 
 
 
-data = [ { "ParticipantID": "Test99", "TrialNum": 0, "Vis": "barchart_vert", "TruePercent": null, "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 1, "Vis": "barchart_vert", "TruePercent": "1.5185185185185186", "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 2, "Vis": "barchart_vert", "TruePercent": "1.1176470588235294", "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 3, "Vis": "barchart_vert", "TruePercent": "1.0740740740740742", "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 4, "Vis": "barchart_vert", "TruePercent": "1.6666666666666667", "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 5, "Vis": "barchart_vert", "TruePercent": "1.6842105263157894", "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 6, "Vis": "barchart_vert", "TruePercent": "2.5", "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 7, "Vis": "barchart_vert", "TruePercent": "1.7272727272727273", "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 8, "Vis": "barchart_vert", "TruePercent": "3.125", "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 9, "Vis": "barchart_vert", "TruePercent": "2.2", "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 10, "Vis": "barchart_hor", "TruePercent": "1.1794871794871795", "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 11, "Vis": "barchart_hor", "TruePercent": "1.121212121212121", "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 12, "Vis": "barchart_hor", "TruePercent": "1.2894736842105263", "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 13, "Vis": "barchart_hor", "TruePercent": "2.45", "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 14, "Vis": "barchart_hor", "TruePercent": "1.8461538461538463", "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 15, "Vis": "barchart_hor", "TruePercent": "7.6", "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 16, "Vis": "barchart_hor", "TruePercent": "2.6666666666666665", "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 17, "Vis": "barchart_hor", "TruePercent": "4", "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 18, "Vis": "barchart_hor", "TruePercent": "6.714285714285714", "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 19, "Vis": "barchart_hor", "TruePercent": "1", "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 0, "Vis": "angles", "TruePercent": null, "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 1, "Vis": "angles", "TruePercent": "1.5185185185185186", "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 2, "Vis": "angles", "TruePercent": "1.1176470588235294", "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 3, "Vis": "angles", "TruePercent": "1.0740740740740742", "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 4, "Vis": "angles", "TruePercent": "1.6666666666666667", "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 5, "Vis": "angles", "TruePercent": "1.6842105263157894", "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 6, "Vis": "angles", "TruePercent": "2.5", "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 7, "Vis": "angles", "TruePercent": "1.7272727272727273", "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 8, "Vis": "angles", "TruePercent": "3.125", "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 9, "Vis": "angles", "TruePercent": "2.2", "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 10, "Vis": "angles", "TruePercent": "1.1794871794871795", "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 11, "Vis": "angles", "TruePercent": "1.121212121212121", "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 12, "Vis": "angles", "TruePercent": "1.2894736842105263", "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 13, "Vis": "angles", "TruePercent": "2.45", "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 14, "Vis": "angles", "TruePercent": "1.8461538461538463", "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 15, "Vis": "angles", "TruePercent": "7.6", "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 16, "Vis": "angles", "TruePercent": "2.6666666666666665", "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 17, "Vis": "angles", "TruePercent": "4", "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 18, "Vis": "angles", "TruePercent": "6.714285714285714", "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 19, "Vis": "angles", "TruePercent": "1", "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 0, "Vis": "volume", "TruePercent": null, "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 1, "Vis": "volume", "TruePercent": null, "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 2, "Vis": "volume", "TruePercent": "121", "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 3, "Vis": "volume", "TruePercent": "169", "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 4, "Vis": "volume", "TruePercent": "64", "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 5, "Vis": "volume", "TruePercent": "81", "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 6, "Vis": "volume", "TruePercent": "144", "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 7, "Vis": "volume", "TruePercent": "144", "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 8, "Vis": "volume", "TruePercent": "36", "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 9, "Vis": "volume", "TruePercent": "81", "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 10, "Vis": "volume", "TruePercent": "36", "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 11, "Vis": "volume", "TruePercent": "64", "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 12, "Vis": "volume", "TruePercent": "81", "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 13, "Vis": "volume", "TruePercent": "121", "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 14, "Vis": "volume", "TruePercent": "81", "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 15, "Vis": "volume", "TruePercent": "25", "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 16, "Vis": "volume", "TruePercent": "36", "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 17, "Vis": "volume", "TruePercent": "144", "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 18, "Vis": "volume", "TruePercent": "49", "ReportedPercent": 12 }, { "ParticipantID": "Test99", "TrialNum": 19, "Vis": "volume", "TruePercent": "25", "ReportedPercent": 12 } ]
 
-csvWriter
-    .writeRecords(data)
-    .then(()=> console.log('The CSV file was written successfully'));
+
--- errorCalc.js
+++ errorCalc.js
@@ -1,89 +1,11 @@
+//Parse through CSV calculate error score for each test
+//Return array: [[VIS, ERRORSCORE]]
 
 
-// copied this from online but feel like theres a different way to parse
-//theres a parse library we could import but didn't want to until i talked with team
-
-
-function parseCSV(file, delimiter, callback) {
-    let reader = new FileReader();
-  
-    // When the FileReader has loaded the file...
-    reader.onload = function() {
-    
-      // Split the result to an array of lines
-      let lines = this.result.split('\n');
-      
-      // Split the lines themselves by the specified
-      // delimiter, such as a comma
-      let result = lines.map(function(line) {
-          return line.split(delimiter);
-      });
-      
-      // As the FileReader reads asynchronously,
-      // we can't just return the result; instead,
-      // we're passing it to a callback function
-      callback(result);
-    };
-    
-    // Read the file content as a single string
-    reader.readAsText(file);
-  }
-
-
-
-  function errorCalc(csvFile){
-
-    let errorArr = [];
-    let rowList = csvFile.lines();
-    // let csvContent = "insert csv file.csv";
-
-    for(let r = 0; r < rowList.length; r++  ) {
-        let row = rowList[ r ];
-        let vis = row.getElementsByClassName("Vis")[0].value;
-        let truePercent = row.getElementsByClassName('TruePercent')[0].value;
-        let reportedPercent = row.getElementsByClassName('ReportedPercent')[0].value;
-
-         // error calculations here for each row
-        let errorScore = Math.abs(Math.log2(reportedPercent/truePercent));
-        errorScore.push([vis, errorScore]);
-
+Papa.parse("csvdata/0225202114.csv", {
+    complete: function(results) {
+        console.log("Finished:", results.data);
     }
+});
 
-    console.log(errorArr);
-}
-  
-
-
-
-
-// function errorCalc(csvFile) {
-
-//         let rows = []
-
-//         //generate paticipantID
-//         //generate trialnum
-//         //get vis
-//         let randomID = "Test" + Math.floor(Math.random() * 100)
-//         //fill barchart vert
-
-//         for(let i = 0; i < 10; i++){
-
-//             rows.push({"ParticipantID": randomID, "TrialNum": i, "Vis": "barchart_vert", "TruePercent": barchartData[i][0], "ReportedPercent": barchartData[i][1]})
-//         }
-//         //fill barchart hor
-//         for(let i = 10; i < 20; i++){
-
-//             rows.push({"ParticipantID": randomID, "TrialNum": i, "Vis": "barchart_hor",  "TruePercent": barchartData[i][0], "ReportedPercent": barchartData[i][1]})
-//         }
-//         //fill angle
-//         for(let i = 0; i < 20; i++){
-
-//             rows.push({"ParticipantID": randomID, "TrialNum":  i, "Vis": "angles",  "TruePercent": angleData[i][0], "ReportedPercent": angleData[i][1]})
-//         }
-//         //fill volume
-//         for(let i = 0; i < 20; i++){
-
-//             rows.push({"ParticipantID": randomID, "TrialNum": i, "Vis":  "volume", "TruePercent": volumeData[i][0], "ReportedPercent": volumeData[i][1]})
-//         }
-
-// }
+//Create a CSV with error data
